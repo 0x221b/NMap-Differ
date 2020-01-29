5599904a62923ae754f434fcabf0223f18b28ce2
0x221b <53126876+0x221b@users.noreply.github.com>
2020-01-29 12:35:13 +0000

    Update nmapdiff.py

--- nmapdiff.py
+++ nmapdiff.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-import sys, os
+import sys, os, filecmp
 from datetime import date
+
+def slack():
+   os.system('touch /opt/diff.txt')
 
 def main(argv):
    #Variables
@@ -10,7 +13,7 @@ def main(argv):
    flags = ''
    ip = ''
    
-   #take in arguments
+   #Take in arguments
    try:
       opts, args = getopt.getopt(argv,"hf:i:",["flags=","ip="])
    except getopt.GetoptError:
@@ -27,13 +30,16 @@ def main(argv):
    print ('flags are "', flags)
    print ('IP is "', ip)
    
-   #call nmap with args
+   #Call nmap with args
    os.system('nmap ' + flags + ' ' + ip + ' -oX /opt/nmap_diff/scan_'+today+' > /dev/null 2>&1')
 
-   #diff with previous day
-
-   #if difference exists send message to slack (poss as seperate function)
-
+   #Diff with previous day
+   ft = '/opt/nmap_diff/scan_'+today
+   fy = '/opt/nmap_diff/scan_'+yesterday
+   
+   #If difference exists send message to slack (poss as seperate function)
+   if filecmp.cmp(ft, fy) == True:
+      slack()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
